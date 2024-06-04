be90e4903d405c79b3ddcbec5c03f05bb57e5dd4
sidraamjad <64356507+sidraamjad@users.noreply.github.com>
2024-06-04 21:50:57 +0500

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "duffy" && b=="15-03-2000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
